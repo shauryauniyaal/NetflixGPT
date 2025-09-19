ca11331791445a743b4d560c01245a0253b4a831
shauryauniyaal <shauryauniyal8@gmail.com>
2025-09-19 11:50:32 +0530

    fixing vulnerabilities

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_3bjvO_Yc8xr7LQ62IA5kWjw5zGwqGGU",
-  authDomain: "netflixgpt-8c1fb.firebaseapp.com",
-  projectId: "netflixgpt-8c1fb",
-  storageBucket: "netflixgpt-8c1fb.firebasestorage.app",
-  messagingSenderId: "345903065956",
-  appId: "1:345903065956:web:07b4275b029ba893b96871",
-  measurementId: "G-D4VHJT9BMZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
